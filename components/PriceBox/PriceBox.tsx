b636dedd99c564a34f68f69f615e0f8a97d1748c
Shekoofeh <shekoofeh@Shekoofehs-MacBook-Pro.local>
2022-08-24 18:30:47 +0430

    fix bug

--- components/PriceBox/PriceBox.tsx
+++ components/PriceBox/PriceBox.tsx
@@ -1,43 +1,43 @@
-import ListItem from 'components/ListItem/ListItem';
-import useTranslation from 'next-translate/useTranslation';
+import ListItem from "components/ListItem/ListItem";
+import useTranslation from "next-translate/useTranslation";
 
 type PriceBoxProps = {
   data: {
     price: number;
     items: string[];
-    status: 'active' | 'disabled' | any;
-    type: 'gold' | 'silver' | 'bronze' | any;
+    status: "active" | "disabled" | any;
+    type: "gold" | "silver" | "bronze" | any;
   };
 
   onSubmit: (value: string) => void;
 };
 
 const PriceBox: React.FC<PriceBoxProps> = ({ data, onSubmit }) => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   return (
     <div className={`price-box ${data.status}`}>
       <div className={`type ${data.type} mb-5 font-extrabold`}>
         <p>{t(data.type)}</p>
       </div>
       {data.items.map((item) => {
-        return <ListItem item={item} />;
+        return <ListItem key={item} item={item} />;
       })}
-      {data.status !== 'disabled' ? (
+      {data.status !== "disabled" ? (
         <div className="price my-4 font-extrabold">
           <p> {data.price.toLocaleString()} T</p>
-          <p> {t('monthly')} </p>
+          <p> {t("monthly")} </p>
         </div>
       ) : (
         <div className="price mt-4 my-5 font-extrabold">
-          <p> {t('coming-soon')}</p>
+          <p> {t("coming-soon")}</p>
         </div>
       )}
       <div>
         <button
           className="w-full primary"
-          disabled={data.status === 'disabled'}
+          disabled={data.status === "disabled"}
         >
-          {t('buy')}
+          {t("buy")}
         </button>
       </div>
     </div>
