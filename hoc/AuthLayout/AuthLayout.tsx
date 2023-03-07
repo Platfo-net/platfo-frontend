fc8f62dab2189cb9436f578f39015455f3ebec2c
Alioo11 <110mors110@gmail.com>
2023-03-07 17:29:52 +0330

    support : change botinow logo with platfo logo

--- hoc/AuthLayout/AuthLayout.tsx
+++ hoc/AuthLayout/AuthLayout.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from 'next';
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
-import PlatfoLogo1 from '../../assets/svg/platfo-logo1.svg';
+import img from 'assets/img/platfo-logo-N.png'
+import Image from 'next/image'
+
 interface Props {
   children: ReactNode;
   className?: string;
@@ -12,7 +14,7 @@ const AuthLayout: NextPage<Props> = ({ children, className }) => {
       <div className="authBodyFrom m-auto">
         <Link href="/">
           <span className="logo flex-col items-center justify-center w-32 mx-auto mt-8">
-            <PlatfoLogo1 />
+            <Image src={img.src} width={100} height={100} className="w-12 px-1 mx-1" />
           </span>
         </Link>
         {children}
--- hoc/DashboardLayout/components/DashboardSideDrawer/components/Logo.tsx
+++ hoc/DashboardLayout/components/DashboardSideDrawer/components/Logo.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import React from "react";
-import PlatfoLogo1 from "../../../../../assets/svg/platfo-logo1.svg";
+import img from 'assets/img/platfo-logo-N.png'
+import Image from 'next/image'
+
 
 const Logo = ({href = "/"}) => {
   return (
     <Link href={href}>
       <span className="logo flex-col items-center justify-center  w-full ">
-        <PlatfoLogo1 />
+      <Image src={img.src} width={100} height={100} className="w-12 px-1 mx-1" />
       </span>
     </Link>
   );
